002c081ebee5219eb916a9376fe8c82fd3b8f3f3
TaegeunYou <dbxorms2310@naver.com>
2024-09-16 15:25:56 +0900

    chore: project initial setup

---
+++ src/test/java/com/sbukak/SBukakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbukak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBukakBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
