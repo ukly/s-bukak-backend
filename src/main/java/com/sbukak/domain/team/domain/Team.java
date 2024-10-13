80f3696ee6551cf72d1d9f85cc64d3f2a0ab86bd
TaegeunYou <dbxorms2310@naver.com>
2024-10-13 21:21:24 +0900

    feat: add team domain

---
+++ src/main/java/com/sbukak/domain/team/domain/Team.java
@@ -0,0 +1,115 @@
+package com.sbukak.domain.team.domain;
+
+import com.sbukak.global.enums.SportType;
+import com.sbukak.domain.team.dto.TeamDto;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "team")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "college_id", nullable = false)
+    private College college;
+
+    @Column(name = "name", nullable = false)
+    private String name;    //팀 이름
+
+    @Column(name = "sport_type", nullable = false)
+    private SportType sportType;    //종목 타입
+
+    @Column(name = "ranking", nullable = false)
+    private int ranking;    //순위
+
+    @Column(name = "points", nullable = false)
+    private int points;    //승점
+
+    @Column(name = "wins", nullable = false)
+    private int wins;    //승
+
+    @Column(name = "draws", nullable = false)
+    private int draws;    //무
+
+    @Column(name = "losses", nullable = false)
+    private int losses;    //패
+
+    @Column(name = "matches", nullable = false)
+    private int matches;    //경기수
+
+    @Column(name = "goals", nullable = false)
+    private int goals;    //득점
+
+    @Column(name = "goals_difference", nullable = false)
+    private int goalsDifference;    //골득실
+
+    @Column(name = "recent_matches", nullable = false)
+    private String recentMatches;    //최근전적
+
+    @Column(name = "icon_image_url", nullable = false)
+    private String iconImageUrl;    //아이콘 이미지 url
+
+    @Column(name = "formation_image_url", nullable = false)
+    private String formationImageUrl;    //포메이션 이미지 url
+
+    @Builder
+    public Team(
+        Long id,
+        String name,
+        SportType sportType,
+        int ranking,
+        int points,
+        int wins,
+        int draws,
+        int losses,
+        int matches,
+        int goals,
+        int goalsDifference,
+        String recentMatches,
+        String iconImageUrl,
+        String formationImageUrl
+    ) {
+        this.id = id;
+        this.name = name;
+        this.sportType = sportType;
+        this.ranking = ranking;
+        this.points = points;
+        this.wins = wins;
+        this.draws = draws;
+        this.losses = losses;
+        this.matches = matches;
+        this.goals = goals;
+        this.goalsDifference = goalsDifference;
+        this.recentMatches = recentMatches;
+        this.iconImageUrl = iconImageUrl;
+        this.formationImageUrl = formationImageUrl;
+    }
+
+    public TeamDto toTeamDto() {
+        return new TeamDto(
+            id,
+            ranking,
+            college.getLeague().getName(),
+            college.getName(),
+            name,
+            points,
+            wins,
+            draws,
+            losses,
+            matches,
+            goals,
+            goalsDifference,
+            recentMatches,
+            iconImageUrl,
+            formationImageUrl
+        );
+    }
+}
---
+++ src/main/java/com/sbukak/domain/team/service/TeamService.java
@@ -0,0 +1,32 @@
+package com.sbukak.domain.team.service;
+
+import com.sbukak.domain.schedule.domain.Schedule;
+import com.sbukak.domain.schedule.repository.ScheduleRepository;
+import com.sbukak.domain.team.domain.Team;
+import com.sbukak.domain.team.dto.GetTeamResponseDto;
+import com.sbukak.domain.team.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+    private final ScheduleRepository scheduleRepository;
+
+    @Transactional(readOnly = true)
+    public GetTeamResponseDto getTeam() {
+        Long teamId = 1L;
+        Team team = teamRepository.findById(teamId)
+            .orElseThrow(() -> new IllegalArgumentException("team not found"));
+        List<Schedule> schedules = scheduleRepository.findAllByTeam(team);
+        return null;
+    }
+
+
+
+}
