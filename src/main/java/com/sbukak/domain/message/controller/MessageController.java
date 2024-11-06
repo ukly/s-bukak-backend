7c73910042a7155a9e2ce7497c609e88bec930a5
ukly <dlwjddnr5438@kookmin.ac.kr>
2024-11-06 21:31:24 +0900

    feat: message기능 기초

---
+++ src/main/java/com/sbukak/domain/message/controller/MessageController.java
@@ -0,0 +1,10 @@
+package com.sbukak.domain.message.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+
+
+}
