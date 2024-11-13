74f424f79aa2f0c65666828cdda1f58ab8fc6646
TaegeunYou <dbxorms2310@naver.com>
2024-11-13 16:40:28 +0900

    refactor(RankingService): 랭킹 목록 정렬 하도록 수정

--- src/main/java/com/sbukak/domain/ranking/service/RankingService.java
+++ src/main/java/com/sbukak/domain/ranking/service/RankingService.java
@@ -1,5 +1,6 @@
 package com.sbukak.domain.ranking.service;
 
+import com.sbukak.domain.team.dto.TeamDto;
 import com.sbukak.global.enums.SportType;
 import com.sbukak.domain.ranking.dto.GetRankingResponseDto;
 import com.sbukak.domain.team.domain.Team;
@@ -7,6 +8,9 @@ import com.sbukak.domain.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +20,10 @@ public class RankingService {
 
     @Transactional(readOnly = true)
     public GetRankingResponseDto getRanking(SportType sportType) {
-        return new GetRankingResponseDto(
-            teamRepository.findAllBySportType(sportType).stream().map(Team::toTeamDto).toList()
-        );
+        List<TeamDto> teams = teamRepository.findAllBySportType(sportType).stream()
+            .map(Team::toTeamDto)
+            .sorted(Comparator.comparing(TeamDto::ranking))
+            .toList();
+        return new GetRankingResponseDto(teams);
     }
 }
