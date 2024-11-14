cd5328afeda3c9ee363ff6234ed002bdf660cb3e
TaegeunYou <dbxorms2310@naver.com>
2024-11-14 15:37:02 +0900

    refactor(GetBoardsRequestDto): 필요없는 필드 삭제

--- src/main/java/com/sbukak/domain/board/dto/GetBoardsRequestDto.java
+++ src/main/java/com/sbukak/domain/board/dto/GetBoardsRequestDto.java
@@ -12,23 +12,17 @@ public class GetBoardsRequestDto {
     private SportType sportType = SportType.SOCCER;
     private BoardType boardType = BoardType.FREE;
     private String query;
-    private int page = 0;
-    private int size = 10;
     private boolean myBoardsOnly = false;
 
     public GetBoardsRequestDto(
         SportType sportType,
         BoardType boardType,
         String query,
-        int page,
-        int size,
         boolean myBoardsOnly
     ) {
         this.sportType = sportType;
         this.boardType = boardType;
         this.query = query;
-        this.page = page;
-        this.size = size;
         this.myBoardsOnly = myBoardsOnly;
     }
 }
