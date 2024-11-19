214292b61da44fb241780e3038edcdb692ca61d1
ukly <dlwjddnr5438@kookmin.ac.kr>
2024-11-19 16:24:36 +0900

    fix: 응웜메시지 팀별 분류

--- src/main/java/com/sbukak/domain/message/controller/MessageController.java
+++ src/main/java/com/sbukak/domain/message/controller/MessageController.java
@@ -2,17 +2,24 @@ package com.sbukak.domain.message.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class MessageController {
 
-    private KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    @MessageMapping("/send")
-    public void sendMessage(String message) {
-        kafkaTemplate.send("your-topic-name", message);
+    @MessageMapping("/send/all")
+    public void sendAllMessage(@Payload String message) {
+        kafkaTemplate.send("all-cheer-messages", message);
+    }
+
+    @MessageMapping("/send/team/{teamId}")
+    public void sendTeamMessage(@Payload String message, @DestinationVariable String teamId) {
+        kafkaTemplate.send("team-cheer-messages", teamId, message);
     }
 }
