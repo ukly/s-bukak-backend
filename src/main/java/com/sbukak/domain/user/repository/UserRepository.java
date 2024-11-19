1c4d59600b819a14c5dff60d321c57902ee54267
ukly <dlwjddnr5438@kookmin.ac.kr>
2024-11-19 22:19:44 +0900

    fix: college lazy load 관련 쿼리 수정

--- src/main/java/com/sbukak/domain/user/repository/UserRepository.java
+++ src/main/java/com/sbukak/domain/user/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sbukak.domain.user.repository;
 
 import com.sbukak.domain.user.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    @EntityGraph(attributePaths = {"team", "team.college"})
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
