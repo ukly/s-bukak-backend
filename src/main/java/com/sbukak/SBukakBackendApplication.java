e4ec4e6a3deb91c5ad800e0e1e38693f41856051
TaegeunYou <dbxorms2310@naver.com>
2024-11-24 15:49:51 +0900

    refactor: timezone Asia/Seoul로 설정

--- src/main/java/com/sbukak/SBukakBackendApplication.java
+++ src/main/java/com/sbukak/SBukakBackendApplication.java
@@ -1,13 +1,21 @@
 package com.sbukak;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class SBukakBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SBukakBackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
