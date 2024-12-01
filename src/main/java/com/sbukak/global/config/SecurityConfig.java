6ce7143e52717c06a8f7d2e5d9b8120fb0d73877
TaegeunYou <dbxorms2310@naver.com>
2024-12-01 17:24:40 +0900

    refactor(SecurityConfig): 게시판 상세 조회 권한 수정

--- src/main/java/com/sbukak/global/config/SecurityConfig.java
+++ src/main/java/com/sbukak/global/config/SecurityConfig.java
@@ -11,13 +11,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -45,7 +43,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authz -> authz
                         .requestMatchers("/ws-chat/**","/", "/swagger", "/swagger-ui/**", "/api-docs/**", "/admin/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/schedule", "/ranking", "/message/**", "/schedule", "/team" +
-                            "/**", "/boards/**", "/banner").permitAll()
+                            "/**", "/boards/**", "/board/**", "/banner").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionMangement -> sessionMangement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
